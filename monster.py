a9382c941e4daaa20298e21201e7db5f8a3aaa9c
dkjang97 <dkjang.97@gmail.com>
2026-07-02 12:01:35 +0900

    Add Mob, Mushroom, and Slime classes

--- monster.py
+++ monster.py
@@ -1,1 +1,62 @@
+class Mob:
+    def __init__(self, name, hp, attack_power, defense):
+        self.name = name
+        self.hp = hp
+        self.attack_power = attack_power
+        self.defense = defense
 
+    def attack(self):
+        print(f"{self.name}의 스킬 발동.")
+        print("공격했습니다.")
+
+    def info(self):
+        print(f"이름 : {self.name}")
+        print(f"HP : {self.hp}")
+        print(f"공격력 : {self.attack_power}")
+        print(f"방어력 : {self.defense}")
+
+class Mushroom(Mob):
+    def run(self):
+        print(f"{self.name}: 달리기")
+
+    def jump(self):
+        print(f"{self.name}: 점프")
+
+class Slime(Mob):
+    def __init__(self, name, hp, attack_power, defense, count=1):
+        super().__init__(name, hp, attack_power, defense)
+        self.count = count
+
+    def split(self):
+        self.count *= 2
+        print(f"{self.name}이(가) 분열했습니다! 현재 개체 수 : {self.count}")
+
+blue_mushroom = Mushroom("파랑버섯🥏:", 25, 4, 1)
+red_mushroom = Mushroom("빨간버섯🍄:", 50, 8, 2)
+
+blue_slime = Slime("파랑슬라임🌀:", 20, 3, 1)
+red_slime = Slime("빨간슬라임🐙:", 40, 6, 2)
+
+
+blue_mushroom.info()
+blue_mushroom.attack()
+blue_mushroom.run()
+blue_mushroom.jump()
+print()
+
+red_mushroom.info()
+red_mushroom.attack()
+red_mushroom.run()
+red_mushroom.jump()
+print()
+
+blue_slime.info()
+blue_slime.attack()
+blue_slime.split()
+blue_slime.split()
+print()
+
+red_slime.info()
+red_slime.attack()
+red_slime.split()
+red_slime.split()
